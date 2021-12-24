a16e3563708579025ec0379ab2f2605e81f94813
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2021-12-24 15:21:22 -0500

    Update Step2_CompileScripts.bat

--- jenkins-ue4-master/build-scripts/Step2_CompileScripts.bat
+++ jenkins-ue4-master/build-scripts/Step2_CompileScripts.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "C:\Source\Capstone_Test\Capstone_Test.sln" /t:build /p:Platform=Win64;verbosity=diagnostic
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "C:\Source\capstonetest\Capstone_Test\Capstone_Test.sln" /t:build /p:Platform=Win64;verbosity=diagnostic
