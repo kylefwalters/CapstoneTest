02f5328f264f76cfa8de9ea5f03b7581492aa32d
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2021-12-24 15:21:01 -0500

    Update Step1_StartBuild.bat

--- jenkins-ue4-master/build-scripts/Step1_StartBuild.bat
+++ jenkins-ue4-master/build-scripts/Step1_StartBuild.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="C:\Source\Capstone_Test\Capstone_Test.uproject" -game -rocket -progress
+"C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="C:\Source\capstonetest\Capstone_Test\Capstone_Test.uproject" -game -rocket -progress
